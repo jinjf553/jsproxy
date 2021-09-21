1ee4ab0f1a0c18f7403e023ce548e7d26acc1819
jinjf553 <32598314+jinjf553@users.noreply.github.com>
2021-09-21 21:10:53 +0800

    update

--- conf.js
+++ conf.js
@@ -32,17 +32,23 @@ jsproxy_config({
         [location.host]: 1,
       }
     },
+    '收费版': {
+      label: '当前站点',
+      lines: {
+        'node-cfworker-2.etherdream.com': 4,
+      }
+    },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
       label: 'cfworker',
       hidden: false,
       lines: {
         // 收费版（高权重）
-        'node-cfworker-2.etherdream.com': 4,
+        // 'node-cfworker-2.etherdream.com': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        // 'cl.jinjf.workers.dev': 1,
+        'cl.jinjf.workers.dev': 1,
         //'b.hehe.workers.dev': 1,
         //'b.lulu.workers.dev': 1,
         //'b.jsproxy.workers.dev': 1,
@@ -53,7 +59,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'cfworker',
+  node_default: '收费版',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
