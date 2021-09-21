acff3ce5b487415eca49a99b014ff51618624cd8
jinjf553 <32598314+jinjf553@users.noreply.github.com>
2021-09-21 20:23:46 +0800

    Update conf.js
    update

--- conf.js
+++ conf.js
@@ -34,7 +34,7 @@ jsproxy_config({
     },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
-      label: '',
+      label: 'cfworker',
       hidden: true,
       lines: {
         // 收费版（高权重）
@@ -42,7 +42,7 @@ jsproxy_config({
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        //'b.007.workers.dev': 1,
+        'cl.jinjf.workers.dev': 1,
         //'b.hehe.workers.dev': 1,
         //'b.lulu.workers.dev': 1,
         //'b.jsproxy.workers.dev': 1,
@@ -53,7 +53,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
+  node_default: 'cfworker',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
